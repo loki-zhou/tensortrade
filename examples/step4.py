e5fc09a2a19b5145cf11097297ad007f2ffb4216
loki-zhou <loki_zhou@hotmail.com>
2023-03-17 00:07:30 +0800

    :>

--- examples/step4.py
+++ examples/step4.py
@@ -79,7 +79,7 @@ def create_env(config):
     x = np.arange(0, 2*np.pi, 2*np.pi / 1001)
     y = 50*np.sin(3*x) + 100
 
-    x = np.arange(0, 2*np.pi, 2*np.pi / 1000)
+    np.arange(0, 2 * np.pi, 2 * np.pi / 1000)
     p = Stream.source(y, dtype="float").rename("USD-TTC")
 
     bitfinex = Exchange("bitfinex", service=execute_order)(
@@ -142,7 +142,7 @@ config={
     "framework": "torch",
     "ignore_worker_failures": True,
     "num_workers": 1,
-    "num_gpus": 0,
+    "num_gpus": 1,
     "clip_rewards": True,
     "lr": 8e-6,
     "lr_schedule": [
@@ -161,50 +161,70 @@ config={
     "entropy_coeff": 0.01
 }
 stop = {
-    "episode_reward_mean": 500,
+    "episode_reward_mean": 700,
 }
-analysis = tune.Tuner(
-    "PPO", param_space=config, run_config=air.RunConfig(stop=stop, checkpoint_config=air.CheckpointConfig(checkpoint_at_end=True))
-).fit()
+if 1:
+    analysis = tune.Tuner(
+        "PPO", param_space=config, run_config=air.RunConfig(stop=stop, checkpoint_config=air.CheckpointConfig(checkpoint_at_end=True))
+    ).fit()
 
-# Get the best result based on a particular metric.
-best_result = analysis.get_best_result(metric="episode_reward_mean", mode="max")
+    # Get the best result based on a particular metric.
+    best_result = analysis.get_best_result(metric="episode_reward_mean", mode="max")
 
-# Get the best checkpoint corresponding to the best result.
-best_checkpoint = best_result.checkpoint
-pprint(best_checkpoint)
+    # Get the best checkpoint corresponding to the best result.
+    best_checkpoint = best_result.checkpoint
+    pprint(best_checkpoint)
+    #Checkpoint(local_path=C:\Users\loki_\ray_results\PPO\PPO_TradingEnv_dbb99_00000_0_2023-03-16_20-34-13\checkpoint_000006)
 
-# analysis = tune.run(
-#     "PPO",
-#     stop={
-#       "episode_reward_mean": 500
-#     },
-#     config={
-#         "env": "TradingEnv",
-#         "env_config": {
-#             "window_size": 25
-#         },
-#         "log_level": "DEBUG",
-#         "framework": "torch",
-#         "ignore_worker_failures": True,
-#         "num_workers": 1,
-#         "num_gpus": 0,
-#         "clip_rewards": True,
-#         "lr": 8e-6,
-#         "lr_schedule": [
-#             [0, 1e-1],
-#             [int(1e2), 1e-2],
-#             [int(1e3), 1e-3],
-#             [int(1e4), 1e-4],
-#             [int(1e5), 1e-5],
-#             [int(1e6), 1e-6],
-#             [int(1e7), 1e-7]
-#         ],
-#         "gamma": 0,
-#         "observation_filter": "MeanStdFilter",
-#         "lambda": 0.72,
-#         "vf_loss_coeff": 0.5,
-#         "entropy_coeff": 0.01
-#     },
-#     checkpoint_at_end=True
-# )
+else:
+    import ray.rllib.agents.ppo as ppo
+    agent = ppo.PPOTrainer(
+        env="TradingEnv",
+        config={
+            "env_config": {
+                "window_size": 25
+            },
+            "framework": "torch",
+            "log_level": "DEBUG",
+            "ignore_worker_failures": True,
+            "num_workers": 1,
+            "num_gpus": 0,
+            "clip_rewards": True,
+            "lr": 8e-6,
+            "lr_schedule": [
+                [0, 1e-1],
+                [int(1e2), 1e-2],
+                [int(1e3), 1e-3],
+                [int(1e4), 1e-4],
+                [int(1e5), 1e-5],
+                [int(1e6), 1e-6],
+                [int(1e7), 1e-7]
+            ],
+            "gamma": 0,
+            "observation_filter": "MeanStdFilter",
+            "lambda": 0.72,
+            "vf_loss_coeff": 0.5,
+            "entropy_coeff": 0.01
+        }
+    )
+
+    checkpoint_path = r"C:\Users\loki_\ray_results\PPO\PPO_TradingEnv_349e6_00000_0_2023-03-16_22-59-52\checkpoint_000010"
+    agent.restore(checkpoint_path)
+    #%%
+    # Instantiate the environment
+    env = create_env({
+        "window_size": 25
+    })
+
+    # Run until episode ends
+    episode_reward = 0
+    done = False
+    obs = env.reset()
+
+    while not done:
+        action = agent.compute_single_action(obs)
+        obs, reward, done, _, info = env.step(action)
+        print("reward", reward)
+        episode_reward += reward
+    print("episode_reward = ", episode_reward)
+    env.render()
